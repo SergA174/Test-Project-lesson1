faf35cafd6d67ecc19e69344e4ea7f27ad0a531a
SergA174 <116829429+SergA174@users.noreply.github.com>
2022-11-05 23:15:19 +0500

    приветствие на любой размер переменной

---
+++ Example005_NewHello/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "арина")
+{
+    Console.WriteLine("Ура, это же Арина! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
