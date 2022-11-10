3cd1ca431798a88ba53c48236484f20bc077c17b
SergA174 <116829429+SergA174@users.noreply.github.com>
2022-11-10 21:20:40 +0500

    заполнение массива произвольными с помощью функции не возвращения

---
+++ Example011_WorkElementMassiv/Program.cs
@@ -0,0 +1,30 @@
+﻿//  войд-метод не возвращения.
+void FillArray(int[] collection)
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1,10);
+        // index = index +1; - замена сокращенным значением
+        index++; 
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        // без Line выводит горизонтально
+        Console.WriteLine(col[position]);
+        position++; 
+    }
+}
+
+int[] array = new int[10];
+// заполняет массив произвольными
+FillArray(array);
+// вывод результата
+PrintArray(array);
