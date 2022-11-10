a00552336341b5c1b30701cbf8a627ae2fe19516
SergA174 <116829429+SergA174@users.noreply.github.com>
2022-11-10 20:16:37 +0500

    поиск индекса первого найденного элемента в массиве по заданному элементу

---
+++ Example010_ArrayElement/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = {17, 84, 46, 5, 37, 32, 58, 73, 5};
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        // останавливает поиск на первом найденном
+        break;
+    }
+    // index = index +1; - замена сокращенным значением
+    index++; 
+}
