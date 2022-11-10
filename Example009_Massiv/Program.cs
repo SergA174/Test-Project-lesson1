9c749424c8f7ed79703484396aabcd283ecb36f1
SergA174 <116829429+SergA174@users.noreply.github.com>
2022-11-10 19:27:53 +0500

    поиск большего числа в массиве

---
+++ Example009_Massiv/Program.cs
@@ -0,0 +1,16 @@
+﻿// поиск наибольшего числа(с помощью массива):
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {83,25,32,47,65,16,71,28,59};
+
+int result = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8]));
+
+Console.WriteLine(result);
